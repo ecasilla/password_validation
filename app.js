70cc6cb802ba4d8a97b2b3d9447e6d93ee00491b
Ernie Casilla <ecasilla@icloud.com>
2014-03-17 15:44:37 -0400

    working commit

--- app.js
+++ app.js
@@ -18,24 +18,22 @@ $(document).ready(function($) {
 
 //Check to see if password is a correct match
     if (patternMatcher(password)) {
-      formdiv.removeClass('has-error has-warning');
-      span.removeClass(" glyphicon-warning-sign glyphicon-remove") ;
+      formdiv.removeClass('has-error has-warning').addClass('has-success');
+      span.removeClass(" glyphicon-warning-sign glyphicon-remove").addClass('glyphicon-ok') ;
 
       $('label.control-label').attr('for', 'inputSuccess2').text('Success');
 
 //Check to see if greater than 8 chars also warn
     } else if (password.length > 8 ){
-
-        formdiv.toggleClass('has-error has-success');
-        span.toggleClass(" glyphicon-warning-sign glyphicon-remove");
-
+      formdiv.removeClass('has-error has-success').addClass('has-warning');
+      span.removeClass(" glyphicon-ok glyphicon-remove").addClass('glyphicon-warning-sign') ;
 
      $('label.control-label').attr('for', 'inputWarning2').text('warning');
 
 //Finally throw Error!
     } else {
-      formdiv.toggleClass('has-success has-warning');
-      span.toggleClass(" glyphicon-warning-sign glyphicon-ok");
+      formdiv.removeClass('has-warning has-success').addClass('has-error');
+      span.removeClass(" glyphicon-warning-sign glyphicon-remove").addClass('glyphicon-remove') ;
 
       $('label.control-label').attr('for', 'inputError2').text('error');
 
